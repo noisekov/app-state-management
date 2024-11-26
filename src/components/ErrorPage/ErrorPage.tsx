ce0b061767db3a953f795b6964a941c56edbfaba
Vladimir <noise123q@mail.ru>
2024-11-26 19:17:45 +0300

    feat: rewrite one class component into functional

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ErrorPage.css';
 
-export default class ErrorPage extends Component {
-    render() {
-        return (
-            <div className="error-page">
-                <h1>Something went wrong</h1>
-                <button
-                    className="button"
-                    onClick={() => window.location.reload()}
-                >
-                    Back to App
-                </button>
-            </div>
-        );
-    }
+export default function ErrorPage() {
+    return (
+        <div className="error-page">
+            <h1>Something went wrong</h1>
+            <button className="button" onClick={() => window.location.reload()}>
+                Back to App
+            </button>
+        </div>
+    );
 }
