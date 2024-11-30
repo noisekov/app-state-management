b420a4ec8a860ce8de0e035da88c59467fa2361f
Vladimir <noise123q@mail.ru>
2024-11-30 21:36:30 +0300

    feat: add react route

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -28,36 +28,40 @@ export default function Pokemon({ onInputData }: PokemonProps) {
     }, [onInputData]);
 
     return (
-        <div className="pokemon-cards">
-            {inputData.map((pokemon) => {
-                const { sprites, name, abilities } = pokemon;
-
-                return (
-                    <div key={name} className="pokemon-card">
-                        <h1>{name ? 'Pokemon' : 'Incorrect input value'}</h1>
-                        {name && sprites && (
-                            <div className="pokemon-card__image">
-                                <img
-                                    src={sprites}
-                                    alt={name}
-                                    width={150}
-                                    height={150}
-                                />
-                            </div>
-                        )}
-                        {!!name && (
-                            <p className="pokemon-card__text">
-                                {name ? `name: ${name}` : ''}
-                            </p>
-                        )}
-                        {!!abilities.length && (
-                            <p className="pokemon-card__text">
-                                abilities: {abilities.join(', ')}
-                            </p>
-                        )}
-                    </div>
-                );
-            })}
+        <div className="pokemon-cards__wrapper">
+            <div className="pokemon-cards">
+                {' '}
+                {inputData.map((pokemon) => {
+                    const { sprites, name, abilities } = pokemon;
+                    return (
+                        <div key={name} className="pokemon-card">
+                            <h1>
+                                {name ? 'Pokemon' : 'Incorrect input value'}
+                            </h1>
+                            {name && sprites && (
+                                <div className="pokemon-card__image">
+                                    <img
+                                        src={sprites}
+                                        alt={name}
+                                        width={150}
+                                        height={150}
+                                    />
+                                </div>
+                            )}
+                            {!!name && (
+                                <p className="pokemon-card__text">
+                                    {name ? `name: ${name}` : ''}
+                                </p>
+                            )}
+                            {!!abilities.length && (
+                                <p className="pokemon-card__text">
+                                    abilities: {abilities.join(', ')}
+                                </p>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ErrorBoundary>
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/class-component/" element={<App />} />
+                </Routes>
+            </BrowserRouter>
         </ErrorBoundary>
     </React.StrictMode>
 );
