50231c7a7067bf8de1952ae7a5ac58f7743499d7
Vladimir <noise123q@mail.ru>
2025-02-14 00:05:34 +0300

    feat: add rtk config

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,13 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { pageSlice } from './pageReducer';
 import { dataSlice } from './dataReducer';
+import { pokemonApi } from '../APISlice/ApiSlice';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
     reducer: {
         page: pageSlice.reducer,
         data: dataSlice.reducer,
+        [pokemonApi.reducerPath]: pokemonApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
