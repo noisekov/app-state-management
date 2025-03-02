bf375d4769614cf10a6b84a31a4cd32fb0954128
noisekov <noise123q@mail.ru>
2025-03-02 20:51:38 +0300

    feat: correct package command

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,8 @@
-import styles from './NotFound.module.css';
-
 export default function NotFound() {
     return (
-        <div className={styles.wrapper}>
+        <>
             <h1>404</h1>
             <p>Page Not Found</p>
-        </div>
+        </>
     );
 }
