195737381b7cff2d8c6de515e46ad5536b0778f1
noisekov <noise123q@mail.ru>
2025-03-09 23:58:40 +0300

    feat: add ssr true

--- src/catchall.tsx
+++ src/catchall.tsx
@@ -1,5 +1,17 @@
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.tsx';
+import { ThemeProvider } from './util/ThemeProvider.tsx';
 
 export default function Component() {
-    return <App />;
+    return (
+        <ErrorBoundary>
+            <ThemeProvider>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </ThemeProvider>
+        </ErrorBoundary>
+    );
 }
