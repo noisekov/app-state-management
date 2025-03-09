b3c103e1f557270a77ad7e9450f9e3c91a9e18fe
noisekov <noise123q@mail.ru>
2025-03-09 23:48:21 +0300

    feat: add 404

--- src/root.tsx
+++ src/root.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
-                <title>My App</title>
+                <title>Pokemon</title>
                 <Meta />
                 <Links />
             </head>
